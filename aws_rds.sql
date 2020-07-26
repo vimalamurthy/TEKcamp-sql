83ee8be3d43f2ca5cdab93971351c611018a93e2
Vimala Murthy <vimala_murthy@yahoo.com>
2020-07-26 18:54:05 -0500

    Complete saved sql files

--- aws_rds.sql
+++ aws_rds.sql
@@ -98,3 +98,99 @@ INSERT INTO ta_Dylan_Fellows
 	VALUES 
 		 (4, "Jessica", "Ulysse", "F", "Bachelors");
 SELECT * FROM ta_Dylan_Fellows ORDER BY first_name;
+CREATE TABLE hobbies (
+	hobbies_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    hobby_name VARCHAR(25),
+    student_id int
+    );
+ALTER TABLE tekcampers 
+	ADD PRIMARY KEY (id);
+ALTER TABLE hobbies
+	ADD FOREIGN KEY (student_id) REFERENCES ta_Dylan_Fellows(id);
+INSERT INTO hobbies (hobby_name)
+ VALUES 
+	("Reading"),
+	("Motorcycle Riding"), 
+    ("Video games"),
+    ("Golfing"),
+    ("Cattle Rustling"),
+    ("Gardening"), 
+    ("Yoga"),
+	("Running"), 
+    ("Video games"),
+    ("Work out"),
+    ("Pickle ball"),
+    ("Projects"), 
+    ("Jiu Jitsu"),
+    ("Writing"),
+    ("Cooking"),
+    ("Eating"),
+    ("Music"),
+	("Hockey"), 
+    ("Soccer"),
+    ("Longboarding"),
+    ("Growing hot peppers"),
+	("Reading"),
+	("Soccer"), 
+    ("Eating"),
+    ("Gaming"),
+    ("Video games")
+    ;
+UPDATE hobbies SET student_id = 28 WHERE hobbies_id = 6;
+UPDATE hobbies SET student_id = 19  WHERE hobbies_id = 1;
+UPDATE hobbies SET student_id = 18  WHERE hobbies_id = 3;
+UPDATE hobbies SET student_id = 17 WHERE hobbies_id = 7;
+UPDATE hobbies SET student_id = 18  WHERE hobbies_id = 9;
+UPDATE hobbies SET student_id = 14  WHERE hobbies_id = 10;
+UPDATE hobbies SET student_id = 10 WHERE hobbies_id = 2;
+UPDATE hobbies SET student_id = 6  WHERE hobbies_id = 4;
+UPDATE hobbies SET student_id = 2  WHERE hobbies_id = 5;
+UPDATE hobbies SET student_id = 34 WHERE hobbies_id = 8;
+UPDATE hobbies SET student_id = 12  WHERE hobbies_id = 11;
+UPDATE hobbies SET student_id = 5  WHERE hobbies_id = 12;
+UPDATE hobbies SET student_id = 7 WHERE hobbies_id = 13;
+UPDATE hobbies SET student_id = 11  WHERE hobbies_id =14;
+UPDATE hobbies SET student_id = 32  WHERE hobbies_id = 15;
+UPDATE hobbies SET student_id = 8 WHERE hobbies_id = 16;
+UPDATE hobbies SET student_id = 4  WHERE hobbies_id = 17;
+UPDATE hobbies SET student_id = 25  WHERE hobbies_id = 18;
+UPDATE hobbies SET student_id = 35 WHERE hobbies_id = 19;
+UPDATE hobbies SET student_id = 21  WHERE hobbies_id = 20;
+UPDATE hobbies SET student_id = 38  WHERE hobbies_id = 21;
+UPDATE hobbies SET student_id = 1 WHERE hobbies_id = 22;
+UPDATE hobbies SET student_id = 16 WHERE hobbies_id = 23;
+UPDATE hobbies SET student_id = 36 WHERE hobbies_id = 24;
+UPDATE hobbies SET student_id = 15 WHERE hobbies_id = 25;
+UPDATE hobbies SET student_id = 30 WHERE hobbies_id = 26;
+INSERT INTO hobbies (hobby_name)
+ VALUES 
+	("Horse riding"),
+    ("Interior decoration"),
+    ("Eat, Sleep & Code");
+UPDATE hobbies SET student_id = 3 WHERE hobbies_id = 27;
+UPDATE hobbies SET student_id = 13 WHERE hobbies_id = 28;
+UPDATE hobbies SET student_id = 29 WHERE hobbies_id = 29;
+SELECT * FROM  hobbies;
+SELECT *
+FROM ta_Dylan_Fellows
+LEFT JOIN hobbies ON ta_Dylan_Fellows.id=hobbies.student_id;
+ALTER TABLE tekcampers ADD bootcamp VARCHAR(25);
+UPDATE tekcampers
+SET bootcamp = 'Coding Dojo'
+WHERE id = 6 OR id = 12 OR id = 7;
+UPDATE tekcampers
+SET bootcamp = 'DevMountain'
+WHERE id = 25 OR id = 17;
+UPDATE tekcampers
+SET bootcamp = 'Trilogy Education'
+WHERE id = 32 OR id = 36;
+UPDATE tekcampers
+SET bootcamp = 'SMU Coding Bootcamp'
+WHERE id = 21;
+UPDATE tekcampers
+SET bootcamp = 'US Navy Bootcamp'
+WHERE id = 2;
+ALTER TABLE ta_Dylan_Fellows ADD bootcamp VARCHAR(25);
+UPDATE ta_Dylan_Fellows 
+SET ta_Dylan_Fellows.bootcamp = tekcampers.bootcamp
+WHERE ta_Dylan_Fellows.id = tekcampers.id; 
